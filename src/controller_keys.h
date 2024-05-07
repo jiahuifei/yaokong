968a5e01a5daf1322658d441b45c929d8d5aff8c
jiahuifei <2022141410209@stu.scu.edu.cn>
2024-05-07 15:26:34 +0800

    第一次提交

---
+++ src/controller_keys.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#define PIN_VOL 4		  // ��ѹ���
+#define PIN_LED 38		  // ����LED
+#define PIN_BUZZER 10	  // ������
+
+#define PIN_LX 2          // ��ҡ��X
+#define PIN_LY 1		  // ��ҡ��Y
+#define PIN_L_KNOB 14	  // ����ť
+#define PIN_L_UP 46		  // ����ϰ���
+#define PIN_L_DOWN 39     // ����°���
+
+#define PIN_RX 16         // ��ҡ��X
+#define PIN_RY 15		  // ��ҡ��Y
+#define PIN_R_KNOB 3	  // ����ť
+#define PIN_R_UP 45		  // �Ҳ��ϰ���
+#define PIN_R_DOWN 44     // �Ҳ��°���
+
+#define PIN_BOARD_L 0     // ������ఴ��
+#define PIN_BOARD_R 21    // �����Ҳఴ��
+
+#define MCP_PIN_L1 8      // �󲦸�1����mcp2307�ϵ�����     ��1---��2---��1---��2
+#define MCP_PIN_L2 11     // �󲦸�2����mcp2307�ϵ�����
+#define MCP_PIN_R1 10     // �Ҳ���1����mcp2307�ϵ�����
+#define MCP_PIN_R2 9      // �Ҳ���2����mcp2307�ϵ�����
+
+#define MCP_PIN_UP 7	  // ���ܼ� ��    
+#define MCP_PIN_DOWN 4	  // ���ܼ� ��    
+#define MCP_PIN_LEFT 6	  // ���ܼ� ��   
+#define MCP_PIN_RIGHT 5   // ���ܼ� ��   
+#define MCP_PIN_O 3	      // ���ܼ� O     
+#define MCP_PIN_X 2	      // ���ܼ� X     
+#define MCP_PIN_A 1	      // ���ܼ� A    
+#define MCP_PIN_B 0	      // ���ܼ� B     
